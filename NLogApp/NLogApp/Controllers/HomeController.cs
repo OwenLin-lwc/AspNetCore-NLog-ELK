415d7c3dd2f123c89397d8a06b74142198b690e6
Owen Lin <owen.lwc@pacificseainvests.com>
2019-08-04 13:48:58 +0800

    add NLog

--- NLogApp/NLogApp/Controllers/HomeController.cs
+++ NLogApp/NLogApp/Controllers/HomeController.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NLogApp.Models;
+using System.Diagnostics;
 
 namespace NLogApp.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private ILogger<HomeController> _logger { get; set; }
         public IActionResult Index()
         {
+            _logger.LogInformation("Home-Index");
+
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation("Home-Privacy");
+
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            _logger.LogInformation("Home-Error");
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- NLogApp/NLogApp/Startup.cs
+++ NLogApp/NLogApp/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace NLogApp
 {
@@ -36,8 +33,10 @@ namespace NLogApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
